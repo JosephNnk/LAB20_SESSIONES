de12ad02c231d14d849cbabe03ddbc19468ac514
JosephNnk <jose.mollo.62353@usalesiana.edu.bo>
2026-05-28 01:33:03 -0400

    Create storage paths before caching

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views storage/logs
+chmod -R 775 storage bootstrap/cache
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
