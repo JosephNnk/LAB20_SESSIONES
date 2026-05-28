5d79a87500c1fa284717dfed78fbac5893822fbc
JosephNnk <jose.mollo.62353@usalesiana.edu.bo>
2026-05-28 02:13:46 -0400

    Force HTTPS and trust proxies on Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+        $middleware->trustProxiesHeaders(Request::HEADER_X_FORWARDED_ALL);
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
             'usuario' => \App\Http\Middleware\EnsureUserIsUser::class,
